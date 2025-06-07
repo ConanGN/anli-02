3cddfe7e20b7707d638e554a9f5ec61c3b7d434f
ConanGN <775482085@qq.com>
2025-06-07 19:42:36 +0800

    动态路由

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='max-w-2xl mx，接下来咱继续完善一下这个需求。'>
+    <div className='max-w-2xl mx-auto'>
       {/* 限制整体宽度 */}
       {children}
     </div>
